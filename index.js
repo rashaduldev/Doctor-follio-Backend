e43a6c95c4fb5ea42e3fb28ccc9e744b6a188de0
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-10-31 16:55:50 +0600

    added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port =process.env.PORT || 3000
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.ioitfil.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,13 +23,35 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const serviceCollection=client.db('carDoctor').collection('service');
+
+    app.get('/service', async(req, res) => {
+      const cursor=serviceCollection.find();
+      const result=await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/service/:id', async(req, res) => {
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, service_id: 1,price: 1 },
+      };
+
+      const result=await serviceCollection.findOne(query,options);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
